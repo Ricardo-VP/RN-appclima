4cb6d2ded21cf2c1f728bc7fef65a01f776e6d3e
RicardoVP2002 <ricardo.vacape@gmail.com>
2021-09-08 19:28:29 -0500

    Add: picker items y animación en botón

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import {
-  SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import Formulario from './components/Formulario';
 
 const App = () => {
-  return(
-  <>
-    <Formulario />
-  </>
-  )
+  const ocultarTeclado = () => {
+    Keyboard.dismiss();
+  }
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => ocultarTeclado()}>
+        <View style={styles.app}>
+          <View style={styles.contenido}>
+            <Formulario />
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: 'rgb(71, 149, 212)',
+    justifyContent: 'center',
+  },
+  contenido: {
+    marginHorizontal: '2.5%',
+  },
+});
 
 export default App;
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,39 +1,85 @@
-import React from 'react';
-import {Text, View, TextInput, StyleSheet, TouchableWithoutFeedback} from 'react-native';
-import { Picker } from '@react-native-community/picker';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Animated,
+} from 'react-native';
+import {Picker} from '@react-native-community/picker';
 
 const Formulario = () => {
+  const [animacionBtn] = useState(new Animated.Value(1));
+  const animacionEntrada = () => {
+    Animated.spring(animacionBtn, {toValue: 0.75, useNativeDriver: true,}).start();
+  };
+  const animacionSalida = () => {
+    Animated.spring(animacionBtn, {
+      toValue: 1,
+      friction: 4,
+      tension: 30,
+      useNativeDriver: true,
+    }).start();
+  };
+  const estiloAnimacion = {
+    transform: [{scale: animacionBtn}],
+  };
   return (
     <>
       <View>
         <View style="styles.formulario">
-          <TextInput placeholder="Ciudad" placeholderTextColor="#666" />
+          <TextInput
+            style={styles.input}
+            placeholder="Ciudad"
+            placeholderTextColor="#666"
+          />
         </View>
         <View>
-            <Picker>
-                <Picker.Item label='-- Seleccione un país --' value=""/>
-                <Picker.Item label='Estados Unidos' value="US"/>
-                <Picker.Item label='México' value="MX"/>
-                <Picker.Item label='Argentina' value="AR"/>
-                <Picker.Item label='Colombia' value="CO"/>
-                <Picker.Item label='Costa Rica' value="CR"/>
-                <Picker.Item label='España' value="ES"/>
-                <Picker.Item label='Ecuador' value="EC"/>
-            </Picker>
+          <Picker itemStyle={{height: 120, backgroundColor: '#FFF'}}>
+            <Picker.Item label="-- Seleccione un país --" value="" />
+            <Picker.Item label="Estados Unidos" value="US" />
+            <Picker.Item label="México" value="MX" />
+            <Picker.Item label="Argentina" value="AR" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Costa Rica" value="CR" />
+            <Picker.Item label="España" value="ES" />
+            <Picker.Item label="Ecuador" value="EC" />
+          </Picker>
         </View>
-        <TouchableWithoutFeedback>
-            <View>
-                <Text>Buscar clima</Text>
-            </View>
+        <TouchableWithoutFeedback
+          onPressIn={() => animacionEntrada()}
+          onPressOut={() => animacionSalida()}>
+          <Animated.View style={[styles.btnBuscar, estiloAnimacion]}>
+            <Text style={styles.txtBuscar}>Buscar clima</Text>
+          </Animated.View>
         </TouchableWithoutFeedback>
       </View>
     </>
   );
 };
 
-const style = StyleSheet.create({
-  formlario: {
-    marginTop: 100,
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    height: 50,
+    backgroundColor: '#FFF',
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  btnBuscar: {
+    marginTop: 50,
+    backgroundColor: '#000',
+    padding: 10,
+    justifyContent: 'center',
+  },
+  txtBuscar: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
 
